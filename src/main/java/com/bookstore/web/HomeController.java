dcb606c80a6cba6df6c3123a31a4c0162d797adf
Dmitry <dmitry.sokolovskyi@gmail.com>
2017-08-21 19:27:17 +0300

    Added index page

--- src/main/java/com/bookstore/web/HomeController.java
+++ src/main/java/com/bookstore/web/HomeController.java
@@ -1,13 +1,13 @@
 package com.bookstore.web;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
     @RequestMapping("/")
-    public String showHomePage() {
-        return "home";
+    public String index() {
+        return "index";
     }
 }
