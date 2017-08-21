a8139bd6fe126752096f44068d5e0bfb44f4b463
Dmitry <dmitry.sokolovskyi@gmail.com>
2017-08-21 19:03:29 +0300

    Created base structure of the project

---
+++ src/main/java/com/bookstore/web/HomeController.java
@@ -0,0 +1,13 @@
+package com.bookstore.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String showHomePage() {
+        return "home";
+    }
+}
