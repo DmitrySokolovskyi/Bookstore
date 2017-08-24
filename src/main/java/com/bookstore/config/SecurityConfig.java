92b5e569237a2f726cd39aee8a85a4097463656f
Dmitry <dmitry.sokolovskyi@gmail.com>
2017-08-24 16:23:06 +0300

    Added Security Config

---
+++ src/main/java/com/bookstore/config/SecurityConfig.java
@@ -0,0 +1,56 @@
+package com.bookstore.config;
+
+import com.bookstore.service.UserSecurityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+    @Autowired
+    private Environment env;
+
+    @Autowired
+    private UserSecurityService userSecurityService;
+
+//    private BCryptPasswordEncoder passwordEncoder() {
+//        return SecurityUtil.passwordEncoder();
+//    }
+
+    private static final String[] PUBLIC_MATCHERS = {
+            "/css/**",
+            "/js/**",
+            "/image/**",
+            "/",
+            "/myAccount"
+    };
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+                .authorizeRequests()
+                .antMatchers(PUBLIC_MATCHERS)
+                .permitAll().anyRequest().authenticated();
+        http
+                .csrf().disable().cors().disable()
+                .formLogin().failureUrl("/login?error").defaultSuccessUrl("/")
+                .and()
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/?logout").deleteCookies("remember-me").permitAll()
+                .and()
+                .rememberMe();
+    }
+
+//    @Autowired
+//    private void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+//        auth.userDetailsService(userSecurityService).passwordEncoder(passwordEncoder());
+//    }
+}
