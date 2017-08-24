6be1282cd738b211784f698cfb4570d49f97be3c
Dmitry <dmitry.sokolovskyi@gmail.com>
2017-08-24 17:10:07 +0300

    Added Login

--- src/main/java/com/bookstore/config/SecurityConfig.java
+++ src/main/java/com/bookstore/config/SecurityConfig.java
@@ -1,19 +1,22 @@
 package com.bookstore.config;
 
 import com.bookstore.service.UserSecurityService;
+import com.bookstore.util.SecurityUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
+@EnableGlobalMethodSecurity(prePostEnabled=true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private Environment env;
@@ -21,9 +24,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
     @Autowired
     private UserSecurityService userSecurityService;
 
-//    private BCryptPasswordEncoder passwordEncoder() {
-//        return SecurityUtil.passwordEncoder();
-//    }
+    private BCryptPasswordEncoder passwordEncoder() {
+        return SecurityUtil.passwordEncoder();
+    }
 
     private static final String[] PUBLIC_MATCHERS = {
             "/css/**",
@@ -34,14 +37,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
     };
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception{
+    protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()
-                .antMatchers(PUBLIC_MATCHERS)
-                .permitAll().anyRequest().authenticated();
+                .authorizeRequests().
+		/*	antMatchers("/**").*/
+                antMatchers(PUBLIC_MATCHERS).
+                permitAll().anyRequest().authenticated();
+
         http
                 .csrf().disable().cors().disable()
                 .formLogin().failureUrl("/login?error").defaultSuccessUrl("/")
+                .loginPage("/login").permitAll()
                 .and()
                 .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                 .logoutSuccessUrl("/?logout").deleteCookies("remember-me").permitAll()
@@ -49,8 +55,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
                 .rememberMe();
     }
 
-//    @Autowired
-//    private void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
-//        auth.userDetailsService(userSecurityService).passwordEncoder(passwordEncoder());
-//    }
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userSecurityService).passwordEncoder(passwordEncoder());
+    }
+
 }
