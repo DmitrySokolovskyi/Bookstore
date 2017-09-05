2e2d3b56652b7e31f7efc92c1db2a6eabfbc82d2
Dmitry <dmitry.sokolovskyi@gmail.com>
2017-09-05 23:04:37 +0300

    Added Update Qty and Delete function in ShoppingCart

--- src/main/java/com/bookstore/web/ShoppingCartController.java
+++ src/main/java/com/bookstore/web/ShoppingCartController.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.security.Principal;
 import java.util.List;
@@ -50,21 +51,40 @@ public class ShoppingCartController {
 
     @RequestMapping("/addItem")
     public String addItem(
-            @ModelAttribute("book")Book book,
+            @ModelAttribute("book") Book book,
             @ModelAttribute("qty") String qty,
             Model model, Principal principal
-            ) {
+    ) {
         User user = userService.findByUsername(principal.getName());
         book = bookService.findOne(book.getId());
 
         if (Integer.parseInt(qty) > book.getInStockNumber()) {
             model.addAttribute("notEnoughStock", true);
-            return "forward:/bookDetail?id=" + book.getId();
+            return "forward:/bookDetail?id="+book.getId();
         }
 
         CartItem cartItem = cartItemService.addBookToCartItem(book, user, Integer.parseInt(qty));
         model.addAttribute("addBookSuccess", true);
 
-        return "forward:/bookDetail?id=" + book.getId();
+        return "forward:/bookDetail?id="+book.getId();
+    }
+
+    @RequestMapping("/updateCartItem")
+    public String updateShoppingCart(
+            @ModelAttribute("id") Long cartItemId,
+            @ModelAttribute("qty") int qty
+    ) {
+        CartItem cartItem = cartItemService.findById(cartItemId);
+        cartItem.setQty(qty);
+        cartItemService.updateCartItem(cartItem);
+
+        return "forward:/shoppingCart/cart";
+    }
+
+    @RequestMapping("/removeItem")
+    public String removeItem(@RequestParam("id") Long id) {
+        cartItemService.removeCartItem(cartItemService.findById(id));
+
+        return "forward:/shoppingCart/cart";
     }
 }
